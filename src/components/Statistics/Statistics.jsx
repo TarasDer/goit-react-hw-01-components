20402608176720072c9189cf8baf8dc526c0ef29
TarasDer <tderev.switch@gmail.com>
2023-05-02 22:37:11 +0300

    add check for task 2

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -5,7 +5,7 @@ import { getRandomHexColor } from 'utils/getRandomHexColor';
 export const Statistics = ({ title, stats }) => {
   return (
     <section className={css.statistics}>
-      <h2 className={css.title}>{title}</h2>
+      {title && <h2 className={css.title}>{title}</h2>}
 
       <ul className={css.stat_list}>
         {stats.map(item => (
